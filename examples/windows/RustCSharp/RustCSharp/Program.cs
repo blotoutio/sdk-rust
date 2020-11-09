04ffc4aa43d53d327ef636ef7bd03e647da7f795
blotouta2 <ankur@blotout.io>
2020-11-09 11:30:13 +0530

    sample application csharp example
    sample application csharp example

---
+++ examples/windows/RustCSharp/RustCSharp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RustCSharp
+{
+    class Program
+    {
+        [DllImport("blotout.dll", EntryPoint = "bo_session_end")] private static extern void bo_session_end();
+        [DllImport("blotout.dll", EntryPoint = "bo_sdk_init")] private static extern void bo_sdk_init(string token, string sdk_end_point,string bundle_id);
+        [DllImport("blotout.dll", EntryPoint = "bo_log_event")] private static extern void bo_log_event(string event_name, string json_string);
+        [DllImport("blotout.dll", EntryPoint = "bo_log_pii_event")] private static extern void bo_log_pii_event(string event_name, string json_string);
+        [DllImport("blotout.dll", EntryPoint = "bo_log_phi_event")] private static extern void bo_log_phi_event(string event_name, string json_string);
+        [DllImport("blotout.dll", EntryPoint = "bo_log_enabled")] private static extern void bo_log_enabled(bool log_enabled);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestSDKInit();
+            TestPublishEvents();
+            TestPublishPHIEvents();
+            TestPublishPIIEvents();
+        }
+
+        private static void TestSDKInit()
+        {
+            
+            string token = "BEZAVGGW4GZZZ3N";
+            string endPoint = "http://stage.blotout.io";
+            string bundleID = "com.blotout.rustsaleDemoApp";
+            //bo_log_enabled(true);
+            bo_sdk_init(token, endPoint, bundleID);
+        }
+
+        private static void TestPublishEvents()
+        {
+            string eventName = "Rust Event from sdk testing";
+
+          string jsonString = "{\"some property\": \"some value\", \"some other property\": \"some other value\"}";
+
+            bo_log_event(eventName,jsonString);
+    }
+
+        private static void TestPublishPIIEvents()
+        {
+            string eventName = "Rust Event from sdk testing";
+
+            string jsonString = "{\"email id\": \"ankuradhikari08@gmail.com\", \"gender\": \"male\"}";
+
+            bo_log_pii_event(eventName, jsonString);
+        }
+
+        private static void TestPublishPHIEvents()
+        {
+            string eventName = "Rust Event from sdk testing";
+
+            string jsonString = "{\"email id\": \"ankur@blotout.io\", \"gender\": \"male\"}";
+
+            bo_log_phi_event(eventName, jsonString);
+        }
+    }
+}
